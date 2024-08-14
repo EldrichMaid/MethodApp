35e82315d06e1f717be6d2ab95eac6630e931895
EldrichMaid <kupo@mail.ru>
2024-08-14 12:17:37 +0300

    5.1.5 Добавлен цикс с выводом значения метода

--- MethodApp/Program.cs
+++ MethodApp/Program.cs
@@ -68,6 +68,17 @@ namespace MethodApp
 
             ShowColor();
 
+            var favcolors = new string[3];
+            for (int i = 0;i < favcolors.Length;i++)
+            {
+                favcolors[i] = ShowColor();
+            }
+            Console.WriteLine("Ваши любимые цвета");
+            foreach (var color in favcolors)
+            {
+                Console.WriteLine(color);
+            }
+
             Console.ReadKey();
         }
     }
