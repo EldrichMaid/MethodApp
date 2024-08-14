72e68d931b5ff91238788948daac6cf1b93ad9aa
EldrichMaid <kupo@mail.ru>
2024-08-14 11:33:31 +0300

    5.0.1 Добавлен кортеж

--- MethodApp/Program.cs
+++ MethodApp/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace MethodApp
+﻿using System.Reflection.Metadata;
+
+namespace MethodApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            (string name, string[] dishes) User;
+            Console.WriteLine("Введите имя");
+            User.name = Console.ReadLine();
+            User.dishes = new string[5];
+            Console.WriteLine("Введите пять любимых блюд");
+            for (int i = 0; i < User.dishes.Length; i++)
+            {
+                User.dishes[i] = Console.ReadLine();
+            }
             
+            Console.ReadKey();
         }
     }
 }
