3780de1236c7a800ba6bb0a066eb747da351619e
EldrichMaid <kupo@mail.ru>
2024-08-14 14:23:39 +0300

    5.2.14 Размер массива в методе изменён на плавающий

--- MethodApp/Program.cs
+++ MethodApp/Program.cs
@@ -39,9 +39,9 @@ namespace MethodApp
             }
             return color;
         }
-        static int[] GetArrayFromConsole()
+        static int[] GetArrayFromConsole(int num = 5)
         {
-            var result = new int[5];
+            var result = new int[num];
 
             for (int i = 0; i < result.Length; i++)
             {
