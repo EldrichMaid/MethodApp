0ae40903b124fb2b2f68b1eae77878b395cd2db8
EldrichMaid <kupo@mail.ru>
2024-08-14 12:45:58 +0300

    5.1.6 Добавлен метод введения массива и его сортировки

--- MethodApp/Program.cs
+++ MethodApp/Program.cs
@@ -39,6 +39,38 @@ namespace MethodApp
             }
             return color;
         }
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            int Local;
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                for (int j = i + 1; j < result.Length; j++)
+                {
+                    if (result[i] > result[j])
+                    {
+                        Local = result[i];
+                        result[i] = result[j];
+                        result[j] = Local;
+                    }
+                }
+            }
+
+            foreach (var counted in result)
+            {
+                Console.Write(counted+"/");
+            }
+
+            return result;
+        }
         static void Main(string[] args)
         {
             (string name, string[] dishes) User;
@@ -78,6 +110,8 @@ namespace MethodApp
             {
                 Console.WriteLine(color);
             }
+
+            GetArrayFromConsole();
 
             Console.ReadKey();
         }
